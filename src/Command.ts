49361e37788531c7b4b67cf36abb9b3428108fef
CorentinTh <corentin.thomasset74@gmail.com>
2020-02-06 22:46:54 +0100

    feat: added default help command

--- src/Command.ts
+++ src/Command.ts
@@ -3,6 +3,6 @@ import {Message} from "discord.js";
 export interface Command {
     name: string,
     description: string,
-    action: (message: Message, args: string[]) => boolean,
-    requiredArgCount: number
+    action: (message: Message, args?: string[]) => boolean|void,
+    requiredArgCount?: number
 }
