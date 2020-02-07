88629d7380b23cfb47513f7bbcb580e6dcd5c711
CorentinTh <corentin.thomasset74@gmail.com>
2020-02-07 10:22:06 +0100

    refactor: indent + static

--- src/Bot.ts
+++ src/Bot.ts
@@ -23,8 +23,8 @@ export class Bot extends Client {
         this.addCommand({
             name: 'help',
             description: 'Print this help message',
-            action: this.helpCommand.bind(this),
-            requiredArgCount:0
+            action: this.sendHelpCommand.bind(this),
+            requiredArgCount: 0
         });
 
         this.config = Object.assign({}, defaultConfig, config);
@@ -38,8 +38,8 @@ export class Bot extends Client {
             if (parser.getCommandName() === `${this.config.prefix}${command.name}`) {
                 if (!command.requiredArgCount || parser.getArgsCount() >= command.requiredArgCount) {
                     command.action(message, parser.getArgs());
-                }else {
-                    this.replyToMessage(message, `Invalid arguments. The command "${command.name}" requires ${command.requiredArgCount} arguments.`);
+                } else {
+                    Bot.replyToMessage(message, `Invalid arguments. The command "${command.name}" requires ${command.requiredArgCount} arguments.`);
                 }
                 return true;
             }
@@ -47,14 +47,14 @@ export class Bot extends Client {
         });
     }
 
-    helpCommand(message: Message) {
+    private sendHelpCommand(message: Message) {
         const spacePaddingLength = 14;
         let helpMessage = 'Available commands:\n\n';
         helpMessage += this.commands.map(command => `${this.config.prefix}${command.name}${' '.repeat(spacePaddingLength - command.name.length)}${command.description}`).join('\n');
-        message.channel.send(helpMessage);
+        Bot.replyToMessage(message, helpMessage);
     }
 
-    replyToMessage(message: Message, content: string): Promise<any> {
+    private static replyToMessage(message: Message, content: string): Promise<any> {
         return message.channel.send(content)
     }
 
--- src/Command.ts
+++ src/Command.ts
@@ -3,6 +3,6 @@ import {Message} from "discord.js";
 export interface Command {
     name: string,
     description: string,
-    action: (message: Message, args?: string[]) => boolean|void,
+    action: (message: Message, args?: string[]) => boolean | void,
     requiredArgCount?: number
 }
